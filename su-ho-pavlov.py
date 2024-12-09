7225b116e8173c55886418cfefc701c81eeae0a9
Михаил Павлов <pavlovma007@gmail.com>
2024-12-09 15:03:44 +0300

    изменил алгоритм: сначала все регистрации, авторизации и все списки должны быть публичны. чтобы потом в них никого не добавляли. Регистратор говорит кто имел право и кто заявился. Агенство говорит кто имеет право голосовать (но показывает их уже поубличным ключам, а другой инф оно все равно не узнает)ю
    потом только голосование в 2 этапа - отправка шифрованных бюлетеней, публикация х, отправка паролей от них, публикация.
    + агенство не считает результаты голосования : любой может их посчитать по публичным данным.
    и не требуется доверять кому то эту важную функцию.

--- su-ho-pavlov.py
+++ su-ho-pavlov.py
@@ -34,7 +34,8 @@ class Registrator:
 		print('Registrator сформировал свои ключи')
 
 	def do_public_elector_list(self):
-		print('Registrator. public MARKSLIST=' , self.MARKSLIST)
+		print('Registrator. Список избирателей (те, кто в принципе имеют право голосовать и им могут быть подписаны ключи)')
+		pprint.pprint(self.MARKSLIST)
 
 	def io_get_pub_key(self) -> bytes:
 		print('Registrator. отдал свой публичный ключ, кому-то, кто запросил')
@@ -79,7 +80,9 @@ class Agency:
 			'public_key': e_public_key_pickled, 
 			'public_key_sign': e_signedHashOfPubKeyByRegistrator, 
 		})
-		print('Agency. проверил подпись публичного ключа избирателя. её подписал действительно Регистратор\nдобавил её в список авторизованных]')
+		print('Agency. этап авторизации ключей избирателя')
+		print('Agency. проверил подпись публичного ключа избирателя. её подписал действительно Регистратор')
+		print('добавил её в список авторизованных')
 
 	def io_get_public_authorized_keys(self):
 		print('Agency. показал список авторизованных ключей')
@@ -107,7 +110,8 @@ class Agency:
 		# всё ок. публикуем . 
 		self.published_encripted_ballots.append(payload)
 		print('\nAgency. проверил подпись Шифрованного бюлетня-ОК, проверил что этот публичный в списке авторизованных')
-		print('добавил шифрованный бюлетень в список опубликованных шифрованных бюлетней :-P у менянет выбора : все ок и яне понимаю что в нём.')
+		print('добавил шифрованный бюлетень в список опубликованных шифрованных бюлетней :-P')
+		print('у меня нет выбора : все ок и я не понимаю что в бюлетене, но судя по всему тот кто её прислал имеет право её прислать и я обязан её принять и опубликовать')
 
 	def io_get_public_published_encripted_ballots(self):
 		print('Agency. выдал список опубликованных бюлетней')
@@ -168,6 +172,17 @@ class Elector:
 		self.mark_1 = random.randint(2, 2 ** 64)
 		self.mark_2 = random.randint(2, 2 ** 64)
 		self.r = r; self.a = a
+
+		# надо пойти ножками в регистратор и сказать им по каким признакам они должны меня авторизовывать у себя. паспорт согласовать например
+		self.r.MARKSLIST.append(self.mark_1)
+		print('Elector. я сходил ножками в Регистратор и убедился, что способ моей авторизации у них согласован')
+		print('для них я буду', self.mark_1)
+		# регистрация ключей и авторизация ключей должна быть выполнена еще до начала голосания, 
+		# чтобы исключить вбросы.
+		self.register_new_keyPair()
+		self.authorize_keys()
+		print('\nElector. я завершил регистрацию своих новых ключей для голосования и авторизовал их еще до начала голосования, чтобы списки ключей тоже были фиксированы чтобы исключить вбросы')
+
 	
 	def register_new_keyPair(self):
 		# содаём и регистрируем подписываем (только Публ) у регистратора "вслепую" пару ключей
@@ -178,7 +193,7 @@ class Elector:
 		# готовим слепую подпись
 		self.hashOfElectorPubKey = hash_of_pub_key(self.public_key)
 		msg = self.hashOfElectorPubKey
-		print('\nElector. hash of my pub_key is\n', msg)
+		# print('\nElector. hash of my pub_key is\n', msg)
 		# загрузили себе публичный ключ
 		if not self.rPubKey:
 			keyInf = pickle.loads(self.r.io_get_pub_key())
@@ -196,7 +211,7 @@ class Elector:
 		if msg != msg_again:
 			raise Exception('Elector. Error. шифрование не дало того эффекта который требуется для голосование. почему ? наверное ваш blind не комутативен')
 
-		print('\nElector. регистратор подписал нам хеш публ ключа, не глядя. подпись=\n', self.signedHashOfPubKeyByRegistrator)
+		print('\nElector. регистратор подписал нам хеш публ ключа, не глядя. вычисленная из всех данных корректная подпись нашего публичного ключа=\n', self.signedHashOfPubKeyByRegistrator)
 	
 	def authorize_keys(self):
 		"у Агенства акторизуем ключи и ждём, чтобы оно публично подтвердило, что ключи приняты"
@@ -220,7 +235,7 @@ class Elector:
 
 	def vote(self, candidate):
 		"создаём бюлетень, шифруем его новым, секретным ключём, и отправляем в агентство пачку шифров"
-		print('Elector. создаём бюлетень, шифруем его новым, секретным ключём, и отправляем в агентство пачку шифров')
+		print('Elector. создаём бюлетень, шифруем его новым, секретным ключём (aes пароль), и отправляем в агентство пачку шифров')
 		# делаем шифро-ключ
 		self.secret_keys = os.urandom(16)
 		# шифровальщик
@@ -262,23 +277,20 @@ r = Registrator() # там возникнет пара ключей "для ко
 a = Agency(r)
 e = Elector(r, a)
 
-# согласовали признаки избирателя на этапе регистрации
-r.MARKSLIST.append(e.mark_1)
+
 r.do_public_elector_list()
+print('Авторизованные ключи Публично (кто-то собирается с их помощью голосовать)', )
+pprint.pprint(a.io_get_public_authorized_keys())
 
 # теперь началось голосование
-print('теперь началось голосование')
+print('\n!!! Теперь Началось Голосование !!!\n')
 
-e.register_new_keyPair()
-e.authorize_keys()
 e.vote("Candidate #2")
 
 
 ### проверка голосов
 print('\nпроверка голосов! \nПубличная информация из которой ...\nЛЮБОЙ ЧУВЫРЛА может САМ посчитать результаты Тайных Цифровых выборов')
 print('')
-print('авторизованные ключи', )
-pprint.pprint(a.io_get_public_authorized_keys())
 print('\nшифрованные бюлетени')
 pprint.pprint(a.io_get_public_published_encripted_ballots())
 print('\nключи от шифрованных бюлетеней')
@@ -307,9 +319,10 @@ for d in list_encripted:
 	secret_key = secret_get_by_mark(mark_2)
 	if secret_key and encripted_ballot:
 		# расшифруем бюлетень
+		print('расшифруем бюлетень')
 		aes = pyaes.AESModeOfOperationCTR(secret_key)
 		# шифруем наше волеизъявление симметрично ключём secret_keys
 		encripted_ballot_bytes = int_to_bytes(encripted_ballot)
 		ballot = aes.decrypt(encripted_ballot_bytes)
-		print('ГОЛОС ЗА КАНДИДАТА:', ballot.decode())
+		print('найден ГОЛОС ЗА КАНДИДАТА:', ballot.decode())
 	
